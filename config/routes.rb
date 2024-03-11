4fef6015bcfa5887823ca8a7f1efb006419e5ba1
Ningjie Cao <kromatiks1@gmail.com>
2024-03-11 12:11:04 +0800

    list controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # A user can see all the lists
+  get "lists", to: "lists#index", as: :lists
+  # A user can create a new list
+  get "lists/new", to: "lists#new", as: :new_list
+  post "lists", to: "lists#create"
+   # A user can see the details of a given list and its name
+   get "lists/:id", to: "lists#show", as: :list
 end
